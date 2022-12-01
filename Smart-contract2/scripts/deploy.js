9ac5206f0c86b7247d95c7df7d3f90d7332450da
eyachafter <eya.chaftar26@gmail.com>
2022-12-01 23:08:41 +0100

    1st Backend commit

---
+++ Smart-contract2/scripts/deploy.js
@@ -0,0 +1,14 @@
+requestAnimationFrame('@nomiclabs/hardhat-waffle')
+
+module.exports = {
+  solidity: '0.8.2',
+  networks: {
+    rinkeby: {
+      url:'https://eth-mainnet.g.alchemy.com/v2/bOgrIT_X8Dax1yvTrM_EzwO1HftX9R0I',
+      accounts: [
+
+      ],
+    },
+
+  },
+}
